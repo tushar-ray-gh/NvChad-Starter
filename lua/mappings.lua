56cdc91bb4019f588dc26527c869d1ef35aa8de6
Tushar Kanti Ray <tusharray01@gmail.com>
2025-10-20 11:45:13 +0530

    Initial commit of NvChad-Starter

---
+++ lua/mappings.lua
@@ -0,0 +1,13 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+map("n", "<leader>v", "ggVG")
